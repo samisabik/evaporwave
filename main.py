18e59b180d41d9578cd6794dfc2e05b6e58800ea
Sami Sabik <sami.sabik@lego.com>
2024-03-10 17:53:35 +0000

    added json file for config

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import json
+from dataclasses import dataclass, field
 import numpy as np
 import cv2 as cv
 import rtmidi as md
-from dataclasses import dataclass, field
 
 @dataclass
 class mask:
     rgb_point: list
-    threshold: int
+    spread: int
     bgr_lowerb: list = field(init=False)
     bgr_upperb: list = field(init=False)
     
@@ -16,12 +17,14 @@ class mask:
     data: int = field(init=False)
     data_max: int = field(init=False)
     data_min: int = field(init=False)
+    data_map: int = field(init=False)
 
-    midi_cc: hex
+    midi_channel: int
+    midi_note: int
     
     def __post_init__(self):
-        self.bgr_lowerb = (np.array(self.rgb_point) - self.threshold).clip(0, 255)[::-1]
-        self.bgr_upperb = (np.array(self.rgb_point) + self.threshold).clip(0, 255)[::-1]
+        self.bgr_lowerb = (np.array(self.rgb_point) - self.spread).clip(0, 255)[::-1]
+        self.bgr_upperb = (np.array(self.rgb_point) + self.spread).clip(0, 255)[::-1]
         self.data_buffer = [0] * nb_frame
     
     def calculate_mask(self, frame, bitwise):
@@ -35,7 +38,7 @@ class mask:
         self.data_buffer.append(self.data)    
         min = np.min(self.data_buffer)
         max = np.max(self.data_buffer)
-        return self.remap(self.data,*(min,max),*(0,127))
+        self.data_map = self.remap(self.data,*(min,max),*(0,127))
     
     def remap(self, val, old_min, old_max, new_min, new_max) -> int:
 
@@ -45,60 +48,84 @@ class mask:
         else:
             return 0
 
-# OpenCV capture init
-cap = cv.VideoCapture('data/evap_001.mov')
-nb_frame = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+# Loading JSON file
+f = open('settings.json')
+settings = json.load(f)
 
+# OpenCV capture init
+cap = cv.VideoCapture('data/' + settings['media'] )
+nb_frame = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+fps = int(cap.get(cv.CAP_PROP_FPS))
+def convert_duration(seconds):
+    minutes = int(seconds // 60)
+    seconds %= 60
+    milliseconds = int((seconds - int(seconds)) * 1000)
+    seconds = int(seconds)
+    return f"{minutes:02d}:{seconds:02d}:{milliseconds:03d}"
+
+# Loading masks
+masks = [0] * len(settings['masks'])
+for idx, m in enumerate(settings['masks']):
+    masks[idx] = mask(m['rgb_value'],m['rgb_spread'],m['midi_channel'],m['midi_note'])  
+    
 # rtMIDI init
 midiout = md.MidiOut()
 midiout.open_port(0)
-
-masks = [
-    mask((227, 145, 42), 100, 0x70),
-    mask((128, 9, 0), 20, 0x71),
-]
+CC_AADR = 0x70
+def midi_start_drone(channel, note):
+    status = 0x90 | (channel - 1)
+    data = [status, note, 127]
+    midiout.send_message(data)
+def midi_stop_drone(channel, note):
+    for m in masks:
+        status = 0x80 | (channel - 1)
+        data = [status, note, 0]
+        midiout.send_message(data)
+def midi_update_drone(channel, data):
+    status = 0xb0 | (channel - 1)
+    data = [status, CC_AADR, data]
+    midiout.send_message(data)
 
 def main():
-    note_on = [0x90, 30, 112]
-    midiout.send_message(note_on)
+        
     toggle_bitwise = True
-
+    
+    for m in masks:
+        midi_start_drone(m.midi_channel,m.midi_note)
+    
     while cap.isOpened():
         ret, frame = cap.read()
         key = cv.waitKey(1)
         
+        frame_id = cap.get(cv.CAP_PROP_POS_FRAMES)
+        remaining_time  = convert_duration((nb_frame / fps)-(frame_id / fps))
+
         if not ret:
             cap.set(cv.CAP_PROP_POS_FRAMES, 0)
             continue
                     
         for m in masks:
             m.calculate_mask(frame, toggle_bitwise)
-            midi_data = m.calculate_point()
+            m.calculate_point()
             h, w, c = m.point_mask.shape
-            cv.rectangle(m.point_mask, (0, h), (w, h - 100), (0,0,0), -1)
-            m.point_mask = cv.putText(m.point_mask, "nPx = " + str(m.data), (20, h - 60), cv.FONT_HERSHEY_SIMPLEX , 1, (200, 200, 200) , 1, cv.LINE_AA)
-            m.point_mask = cv.putText(m.point_mask, "CC_" + str(m.midi_cc) + " = " + str(midi_data), (20,h - 20), cv.FONT_HERSHEY_SIMPLEX , 1, (255, 255, 255) , 1, cv.LINE_AA) 
-            midi_msg = [0xb0, m.midi_cc, midi_data]
-            midiout.send_message(midi_msg)
+            cv.rectangle(m.point_mask, (0, h), (w, h - 60), (0,0,0), -1)
+            m.point_mask = cv.putText(m.point_mask, str("{:07d}".format(m.data)), (30,45), cv.FONT_HERSHEY_SIMPLEX , 1, (200, 200, 200) , 1, cv.LINE_AA)
+            m.point_mask = cv.putText(m.point_mask, " [" + str("{:03d}".format(m.data_map)) + "]", (200,45), cv.FONT_HERSHEY_SIMPLEX , 1, (200, 200, 200) , 1, cv.LINE_AA)
+            midi_update_drone(m.midi_channel,m.data_map)
+
+        frame = cv.putText(frame, str(remaining_time), (30,45), cv.FONT_HERSHEY_SIMPLEX , 1, (0, 0, 0) , 1, cv.LINE_AA)
 
         mix = np.concatenate([frame] + [m.point_mask for m in masks], axis=1)
+        cv.imshow("lucas_evaporwave", mix) 
 
-        cv.imshow("output", mix) 
-       
-        #quit
-        if key & 0xFF == ord('q'):
-            break 
+        match (key & 0xFF):
+            case 113: break 
+            case 112: cv.waitKey(-1) 
+            case 114: cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+            case 116: toggle_bitwise = not toggle_bitwise
         
-        #pause
-        elif key & 0xFF == ord('p'):
-            cv.waitKey(-1) 
-            
-        #toggle bitwise mask with the frame
-        elif key & 0xFF == ord('t'):
-            toggle_bitwise = not toggle_bitwise
-
-    note_off = [0x80, 30, 0]  
-    midiout.send_message(note_off)
+    for m in masks:
+        midi_stop_drone(m.midi_channel,m.midi_note)
 
     cap.release()
     cv.destroyAllWindows()
