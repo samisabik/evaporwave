c3b4a865bc5ae1ee3cfabc2fe6cbad675fd432c0
Sami Sabik <sami.sabik@lego.com>
2024-03-18 18:25:43 +0100

    added auto reload of masks based on json edit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json, time
+import json, time, os
 from dataclasses import dataclass, field
 import numpy as np
 import cv2 as cv
@@ -30,7 +30,7 @@ class mask:
     def __post_init__(self):
         self.bgr_lowerb = (np.array(self.rgb_point) - self.spread).clip(0, 255)[::-1]
         self.bgr_upperb = (np.array(self.rgb_point) + self.spread).clip(0, 255)[::-1]
-        self.data_buffer = [0] * nb_frame
+        self.data_buffer = [0]
         self.midi_enable = False
         self.midi_start = False
         
@@ -55,63 +55,74 @@ class mask:
         else:
             return 0
 
-# Loading JSON file
-f = open('settings.json')
-settings = json.load(f)
+def load_masks(json_path):
+    f = open(json_path)
+    settings = json.load(f)
+    masks = [0] * len(settings['masks'])
+    for idx, m in enumerate(settings['masks']):
+        masks[idx] = mask(m['name'],m['rgb_value'],m['rgb_spread'],m['midi_channel'],m['midi_note'])  
+    return masks
 
-# OpenCV capture init
-cap = cv.VideoCapture('data/' + settings['media'] )
-nb_frame = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
-fps = int(cap.get(cv.CAP_PROP_FPS))
-def convert_duration(seconds):
-    minutes = int(seconds // 60)
-    seconds %= 60
-    milliseconds = int((seconds - int(seconds)) * 1000)
-    seconds = int(seconds)
-    return f"{minutes:02d}:{seconds:02d}:{milliseconds:03d}"
+def load_media(json_path):
+    f = open(json_path)
+    settings = json.load(f)
+    cap = cv.VideoCapture('data/' + settings['media'] )
+    return cap
 
-# Loading masks
-masks = [0] * len(settings['masks'])
-for idx, m in enumerate(settings['masks']):
-    masks[idx] = mask(m['name'],m['rgb_value'],m['rgb_spread'],m['midi_channel'],m['midi_note'])  
-    
-# rtMIDI init
-midiout = md.MidiOut()
-available_ports = midiout.get_ports()
-midiout.open_port(0)
+def midi_init(port):
+    midiout = md.MidiOut()
+    midiout.open_port(port)
+    return midiout
 
-def midi_start_drone(channel, note):
+def midi_start(midi_session, channel, note):
     status = 0x90 | (channel - 1)
     data = [status, note, 127]
-    midiout.send_message(data)
-def midi_stop_drone(channel, note):
-    for m in masks:
-        status = 0x80 | (channel - 1)
-        data = [status, note, 0]
-        midiout.send_message(data)
-def midi_update_drone(channel, data):
+    midi_session.send_message(data)
+    
+def midi_stop(midi_session, channel, note):
+    status = 0x80 | (channel - 1)
+    data = [status, note, 0]
+    midi_session.send_message(data)
+    
+def midi_update(midi_session, channel, data):
     status = 0xb0 | (channel - 1)
     data = [status, 0x01, data]
-    midiout.send_message(data)
+    midi_session.send_message(data)
      
 def main():
-        
+    
+    json_path = 'settings.json'
     toggle_bitwise = True
-
+    toggle_help = False
+    midi_session = midi_init(0)
+    masks = load_masks(json_path)
+    cap = load_media(json_path)
+    last_update = os.stat('settings.json').st_mtime
+    
     while cap.isOpened():
         ret, frame = cap.read()
         key = cv.waitKey(1)
         
+        current_update = os.stat('settings.json').st_mtime
+        
+        if (current_update != last_update):
+            last_update = current_update
+            for m in masks:
+                midi_stop(midi_session,m.midi_channel,m.midi_note)
+            masks = load_masks(json_path)
+            cap = load_media(json_path)
+            for m in masks:
+                midi_start(midi_session,m.midi_channel,m.midi_note)
         if not ret:
             cap.set(cv.CAP_PROP_POS_FRAMES, 0)
             continue
-        
+                
         for idx,m in enumerate(masks):
             m.calculate_mask(frame, toggle_bitwise)
             m.calculate_point()
             h, w, c = m.point_mask.shape
             cv.rectangle(m.point_mask, (0, 0), (w, 110), (0,0,0), -1)
-            m.point_mask = cv.putText(m.point_mask, m.name, (30,45), cv.FONT_ITALIC, 1, (255, 255, 255) , 1, cv.LINE_AA)
+            m.point_mask = cv.putText(m.point_mask, " / " + m.name, (30,45), cv.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255) , 1, cv.LINE_AA)
 
             if (key & 0xFF) == 49 + idx:
                 m.midi_enable = not m.midi_enable
@@ -120,18 +131,20 @@ def main():
             if m.midi_enable:
                 if not m.midi_start:
                     m.midi_start = True
-                    midi_start_drone(m.midi_channel,m.midi_note)
-                m.point_mask = cv.putText(m.point_mask, str("{:07d}".format(m.data)), (30,90), cv.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255) , 1, cv.LINE_AA)
-                m.point_mask = cv.putText(m.point_mask, " [" + str("{:03d}".format(m.data_map)) + "]", (200,90), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255) , 1, cv.LINE_AA)
-                midi_update_drone(m.midi_channel,m.data_map)
+                    midi_start(midi_session,m.midi_channel,m.midi_note)
+                m.point_mask = cv.putText(m.point_mask, " / ch" + str(m.midi_channel) + " / " + str("{:03d}".format(m.data_map)), (30,90), cv.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255) , 1, cv.LINE_AA)
+                midi_update(midi_session,m.midi_channel,m.data_map)
             elif not m.midi_enable and m.midi_start:
                 m.midi_start = False
-                midi_stop_drone(m.midi_channel,m.midi_note)
+                midi_stop(midi_session,m.midi_channel,m.midi_note)
             else:
-                m.point_mask = cv.putText(m.point_mask, "track off", (30,90), cv.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255) , 1, cv.LINE_AA)
-
+                m.point_mask = cv.putText(m.point_mask, " / track off", (30,90), cv.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255) , 1, cv.LINE_AA)
 
         mix = np.concatenate([frame] + [m.point_mask for m in masks], axis=1)
+        if toggle_help:
+            h, w, c = mix.shape
+            cv.rectangle(mix, (200, int(h/2)-200), (w - 200, int(h/2)+200), (0,255,0), -1)
+
         cv.imshow("", mix) 
 
         match (key & 0xFF):
@@ -139,7 +152,11 @@ def main():
             case 112: cv.waitKey(-1) 
             case 114: cap.set(cv.CAP_PROP_POS_FRAMES, 0)
             case 116: toggle_bitwise = not toggle_bitwise
-            
+            case 104: toggle_help = not toggle_help
+
+    for m in masks:
+        midi_stop(midi_session,m.midi_channel,m.midi_note)
+
     cap.release()
     cv.destroyAllWindows()
 
